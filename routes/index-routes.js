ec9d8b66837cdcc991af211496aa5e7decc2d290
Ricky Grams <rickygrams@me.com>
2017-05-19 02:49:36 -0700

    after login goes to dashboard/user.id

--- routes/index-routes.js
+++ routes/index-routes.js
@@ -27,17 +27,6 @@ router.post('/register', passport.authenticate('local-signup', {
       }
   ));
 
- // router.post('/register', function(req, res, next) {
- //      passport.authenticate('local-signup', {failureFlash:true}, function(err, user, info) {
- //       if (err) { return next(err); }
- //       if (!user) { return res.redirect('/login'); }
- //      req.logIn(user, function(err) {
- //        if (err) { return next(err); }
- //       return res.redirect('/dashboard/' + user.id);
- //     });
- //    })(req, res, next);
- //    });
-
 // Render Login Page
 router.get('/login', (req, res) => {
   res.render('login');
